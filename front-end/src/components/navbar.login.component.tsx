beec25c3a5e589d847dd3f735d06e3450f194c5c
chatta2539 <chatta2539@hotmail.com>
2023-05-18 03:21:55 -0700

    Fixed Unauthorized bug (First solution)

--- front-end/src/components/navbar.login.component.tsx
+++ front-end/src/components/navbar.login.component.tsx
@@ -4,9 +4,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
 export default function NavbarLogin() {
-
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
